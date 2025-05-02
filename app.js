681e4f01c257a3fe02960d2579c2fce70a63cb3a
Lola Victoria <damilolaoniyide11@gmail.com>
2025-05-03 00:09:32 +0100

    optimized performnce

--- app.js
+++ app.js
@@ -5,10 +5,26 @@ const cityName = document.getElementById('location-input');
 const searchButton = document.getElementById('search-btn');
 const weatherIcon = document.getElementById('weather-icon');
 const locationBtn = document.getElementById('locationBtn');
-
 const weatherInfo = document.getElementById('weatherInfo');
 
 
+function getWeatherIcon(condition) {
+  switch (condition) {
+    case "Clear":
+      return "images/weather-icons/clear.png";
+    case "Clouds":
+      return "images/weather-icons/clouds.png";
+    case "Drizzle":
+      return "images/weather-icons/drizzle.png";
+    case "Rain":
+    case "Mist":
+      return "images/weather-icons/mist.png";
+    case "Snow":
+      return "images/weather-icons/snow.png";
+    default:
+      return "images/weather-icons/default.png";
+  }
+}
 
 async function checkWeather(city){
     // if(city.length == 0) {
@@ -28,47 +44,21 @@ async function checkWeather(city){
         document.getElementsByClassName('error')[0].style.color = 'red';
         document.getElementById('weather-container').style.display = 'none';       
     } else {
-    const data = await response.json();
-    document.getElementById('weather-container').style.display = 'block';
-    document.getElementsByClassName('error')[0].style.display = 'none';
-  
-    console.log(data)
-    
-    localStorage.setItem('lastCity', city);
-
-    document.querySelector('#city').innerHTML = data.name;
-    document.querySelector("#temperature").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector("#wind").innerHTML = data.wind.speed + "km/h";
-    document.getElementById('weather-description').innerHTML = data.weather[0].description;
-    
-    
-    document.getElementById('weather-icon').src = "img/" + data.weather[0].icon + ".png";
-    const weatherCondition = data.weather[0].main;
-
-switch (weatherCondition) {
-    case "Clear":
-        weatherIcon.src = "images/weather-icons/clear.png";
-        break;
-    case "Clouds":
-        weatherIcon.src = "images/weather-icons/clouds.png";
-        break;
-    case "Drizzle":
-        weatherIcon.src = "images/weather-icons/drizzle.png";
-        break;
-    case "Mist":
-        weatherIcon.src = "images/weather-icons/mist.png";
-        break;
-    case "Snow":
-        weatherIcon.src = "images/weather-icons/snow.png";
-        break;
-    default:
-        weatherIcon.src = "images/weather-icons/default.png"; // fallback image if condition is unrecognized
-    }
-
-
+      const data = await response.json();
+      document.getElementById('weather-container').style.display = 'block';
+      document.getElementsByClassName('error')[0].style.display = 'none';
+      localStorage.setItem('lastCity', city);
+      document.querySelector('#city').innerHTML = data.name;
+      document.querySelector("#temperature").innerHTML = Math.round(data.main.temp) + "°C";
+      document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
+      document.querySelector("#wind").innerHTML = data.wind.speed + "km/h";
+      document.getElementById('weather-description').innerHTML = data.weather[0].description;
+      const weatherCondition = data.weather[0].main;
+      weatherIcon.src = getWeatherIcon(weatherCondition);
     }
 }
+
+
 // Fetch 5-day forecast by coordinates
 function get5DayForecast(lat, lon) {
     fetch(
@@ -80,27 +70,32 @@ function get5DayForecast(lat, lon) {
       })
       .catch(() => {
         weatherInfo.innerHTML = 'Error fetching forecast data.';
-      });
-  }
+    });
+}
 
   // display 5-day forecast by coordinates
-  function display5DayForecast(forecast) {
-    let forecastHTML = '<h3>5-Day Forecast:</h3><div class="forecast-container">';
+function display5DayForecast(forecast) {
+    let forecastHTML = '<div class="forecast"><h3>Your location&apos;s next 5 days forecast:</h3><div class="forecast-container">';
+        
     
     forecast.forEach((entry, index) => {
-      if (index % 8 === 0) {  // Display forecast for every 24 hours (8 measurements per day)
+      if (index % 8 === 0) {  
+        const condition = entry.weather[0].main;
+      const iconSrc = getWeatherIcon(condition);
         forecastHTML += `
           <div class="forecast-item">
-            <p><strong>${new Date(entry.dt * 1000).toLocaleDateString()}</strong></p>
-            <p><img src="https://openweathermap.org/img/wn/${entry.weather[0].icon}.png" alt="weather-icon" /></p>
-            <p><strong>Temp:</strong> ${entry.main.temp} °C</p>
-            <p><strong>Weather:</strong> ${entry.weather[0].description}</p>
+            <p id="date"><strong>${new Date(entry.dt * 1000).toLocaleDateString()}</strong></p>
+            <img loading="lazy" id="weather-icon" src="${iconSrc}" alt="${condition} icon">
+            <div class="temperature-container">
+              <p id="temperature"><strong>Temp:</strong> ${entry.main.temp} °C</p>
+              <p id="weather-description"><strong>Weather:</strong> ${entry.weather[0].description}</p>
+            </div>
           </div>
         `;
       }
     });
-  
-    forecastHTML += '</div>';
+    
+    forecastHTML += '</div></div>';
     weatherInfo.innerHTML += forecastHTML;
   }  
 
@@ -117,24 +112,48 @@ function getWeatherByCoords(lat, lon) {
       .catch(() => {
         weatherInfo.innerHTML = 'Error fetching data.';
       });
+      
   }
+
+ 
   
   // Display current weather data
   function displayWeather(data) {
+    const weatherCondition = data.weather[0].main;
+    const iconSrc = getWeatherIcon(weatherCondition);
+
     weatherInfo.innerHTML = `
-      <h2>${data.name}, ${data.sys.country}</h2>
-      <p><strong>Temperature:</strong> ${data.main.temp} °C</p>
-      <p><strong>Weather:</strong> ${data.weather[0].description}</p>
-      <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
-      <p><strong>Wind Speed:</strong> ${data.wind.speed} m/s</p>
+      <h2 class="city">${data.name}, ${data.sys.country}</h2>
+
+      <div class="current-weather">
+        <img loading="lazy" id="weather-icon" src="${iconSrc}" alt="Weather icon">
+        <div class="temperature-container">
+          <h3 id="temperature"> ${data.main.temp} °C</h3>
+          <p id="weather-description">${data.weather[0].description}</p>
+        </div>
+      </div>
+
+      <div class="weather-details">
+        <div class="detail">
+          <img loading="lazy" id="humidity-icon" src="/images/humidity.png" alt="Humidity icon">
+          <span class="label">Humidity</span>
+          <p> ${data.main.humidity}%</p>
+        </div>
+        <div class="detail">
+          <img loading="lazy" id="wind-icon" src="/images/wind.png" alt="Wind icon">
+          <span class="label">Wind</span>
+          <p> ${data.wind.speed} m/s</p>
+        </div>
+      </div>
     `;
   }
 
-
+// Event listeners for search button and input field
 cityName.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') checkWeather(cityName.value);
   });
 
+  // Search button click event
 searchButton.addEventListener('click', ()=>{
     checkWeather(cityName.value);
   });
