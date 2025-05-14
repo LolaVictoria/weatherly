5708d6181e2600e7bac0358e2d4eaa5767b1fbbe
Lola Victoria <damilolaoniyide11@gmail.com>
2025-05-15 00:47:59 +0100

    new changes

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
 // Force waiting service worker to become active
-workbox.core.skipWaiting();
+workbox.core.self.skipWaiting();
 workbox.core.clientsClaim();
 
 if (workbox) {
