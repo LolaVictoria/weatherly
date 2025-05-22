002abd80ffd34b6a858bb0451640fc2f6c4318b6
Lola Victoria <damilolaoniyide11@gmail.com>
2025-05-22 01:13:03 +0100

    created a new file: install.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { CONFIG } from './config.js';
+import { CONFIG } from '../config.js';
 const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?&appid=${CONFIG.WEATHER_API_KEY}&units=metric&q=`;
 
 const cityName = document.getElementById('location-input');
@@ -184,7 +184,7 @@ function getWeatherByCoords(lat, lon) {
         get5DaysForecast(lat, lon);
       })
       .catch(() => {
-        weatherInfo.innerHTML = 'Error fetching data.';
+        weatherInfo.innerHTML = 'Please turn on your device&apos;s location to get weather data.';
       });
   }
 
@@ -238,56 +238,3 @@ window.onload = () => {
       }
 };
 
-//service workers
-if('serviceWorker' in navigator){
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js').then(reg => {
-        reg.onupdatefound = () => {
-          const newWorker = reg.installing;
-          newWorker.onstatechange = () => {
-            if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-              window.location.reload();
-            }
-          };
-        };
-      });
-      
-    })
- } 
-
-let deferredPrompt;
-
-document.addEventListener('DOMContentLoaded', () => {
-  const installBtn = document.getElementById('installBtn');
-
-  window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    deferredPrompt = e;
-
-    // Show the button
-    installBtn.style.display = 'block';
-
-    installBtn.addEventListener('click', () => {
-      // Directly triggered by user click
-      installBtn.style.display = 'none';
-
-      // Show the install prompt
-      deferredPrompt.prompt();
-
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the install prompt');
-        } else {
-          console.log('User dismissed the install prompt');
-        }
-        deferredPrompt = null;
-      });
-    });
-  });
-
-
-  //Confirm successful installation.
-  window.addEventListener('appinstalled', () => {
-    console.log('PWA was installed');
-  });
-});
---
+++ js/install.js
@@ -0,0 +1,54 @@
+//service workers
+if('serviceWorker' in navigator){
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then(reg => {
+        reg.onupdatefound = () => {
+          const newWorker = reg.installing;
+          newWorker.onstatechange = () => {
+            if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+              window.location.reload();
+            }
+          };
+        };
+      });
+      
+    })
+ } 
+ 
+
+let deferredPrompt;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const installBtn = document.getElementById('installBtn');
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+
+    // Show the button
+    installBtn.style.display = 'block';
+
+    installBtn.addEventListener('click', () => {
+      // Directly triggered by user click
+      installBtn.style.display = 'none';
+
+      // Show the install prompt
+      deferredPrompt.prompt();
+
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+        deferredPrompt = null;
+      });
+    });
+  });
+
+
+  //Confirm successful installation.
+  window.addEventListener('appinstalled', () => {
+    console.log('PWA was installed');
+  });
+})
